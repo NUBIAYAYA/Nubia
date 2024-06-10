ccbfec61eb0e99f1be28a6454359b343f2f64b3b
NubiaY <nubia.yaya@gmail.com>
2024-06-10 03:49:43 -0500

    Selectores2

--- Github/Clase 050624/logica.js
+++ Github/Clase 050624/logica.js
@@ -1,3 +1,20 @@
 
 console.log ('Logica Conectada')
+console.log ('Hola')
 
+const Usuario ='Admin'
+const Contraseña = '1234'
+
+function pruebaBoton () {
+let user = document.getElementById(user)
+
+let pw = document.getElementById(pw).value
+console.log ('Usuario$(user) Contraseña ${pw}')
+if (Usuario== user && Contraseña==pw){
+    window.open('calculadora.html')
+    document.getElementById('Bienvenida').innerHTML('Bienvenido ${user}')
+}else {
+    console.log ('error de credenciales');
+}
+
+}
