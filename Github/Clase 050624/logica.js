f3ca7329693e64a57731152fa8a1880ffd5c72b4
NubiaY <nubia.yaya@gmail.com>
2024-06-11 00:12:17 -0500

    Termino Ejer

--- Github/Clase 050624/logica.js
+++ Github/Clase 050624/logica.js
@@ -7,10 +7,9 @@ const clave = '1234'
 function pruebaBoton(){
     let user = document.getElementById ('user').value
     let pw = document.getElementById ('pw').value
-  if{ usuario == user && clave==pw }
-
-    console.log ('Ingreso correcto');
-    else{
+    if ( usuario==user && clave==pw){
+    console.log ('Ingreso correcto')
+    }else{
         console.log('error en credenciales');
     }
 }
