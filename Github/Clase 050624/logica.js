211144b4a927d7cb4ab60a1a8261853f0053ebe4
NubiaY <nubia.yaya@gmail.com>
2024-06-21 15:42:05 -0500

    Clase18

--- Github/Clase 050624/logica.js
+++ Github/Clase 050624/logica.js
@@ -3,6 +3,7 @@ console.log ('Logica Conectada');
 
 const usuario = 'Admin'
 const clave = '1234'
+const frutas=['Manzana','Pera','sandia']
 
 function pruebaBoton(){
     let user = document.getElementById ('user').value
@@ -12,4 +13,33 @@ function pruebaBoton(){
     }else{
         console.log('error en credenciales');
     }
+   
 }
+function imprimirArreglo() {
+    let posicion= document.getElementById('arrayPosicion').value
+    
+    if(posicion>=frutas.length){
+        console.log('posicion no valida');
+        limpiar()
+        
+    }else{
+
+    } console.log(frutas[posicion]);
+    
+}
+function limpiar() {
+    document.getElementById('arrayPosicion').value= ''
+    document.getElementById('arrayPosicion').focus()
+
+}
+function AgregarFruta(){
+    const NombreFruta = document.getElementById('NombreFruta'). value
+    frutas.push(NombreFruta)
+    console.log('Se Agrega la fruta ${NombreFruta}')
+    document.getElementById('NombreFruta').focus
+
+}
+function EliminarFruta(){
+    console.log ('Eliminar');
+
+    }
